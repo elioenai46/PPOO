13578bc52022ef16bd424cbd9bcfbc91470f450b
elioenai <elioenai.2001@outlook.com>
2022-03-07 11:34:45 -0600

    Este es mi cuarto comit

--- elio.java
+++ elio.java
@@ -1,4 +1,12 @@
 public class holaMundo {	
 public static void main(String []args){
+	System.out.println("     *****");
+	System.out.println("     *   *");
+	System.out.println(" *****   *****");
+	System.out.println(" *           *");
+	System.out.println(" *****   *****");
+	System.out.println("     *   *");
+	System.out.println("     *   *");
+	System.out.println("     *****");
 }
 }
